8ae313bdcd3af098a8f1cc0fa0a8fdb12857dd1a
Sohail <kabeersohel@gmail.com>
2022-10-03 11:11:47 +0530

    auto start working in redmi note 5 pro

--- app/src/main/java/com/example/autostartnow/services/AutoStartAccessibilityService.kt
+++ app/src/main/java/com/example/autostartnow/services/AutoStartAccessibilityService.kt
@@ -6,14 +6,36 @@ import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Toast
 
-class AutoStartAccessibilityService: AccessibilityService() {
+class AutoStartAccessibilityService : AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+//        if (event?.className == "com.android.systemui.media.MediaProjectionPermissionActivity") {
+//            val nodeInfo = this.rootInActiveWindow
+//            for (i in 0 until nodeInfo.childCount) {
+//                val child: AccessibilityNodeInfo? = nodeInfo.getChild(i)
+//                if (child != null) {
+//                    val text = child.text
+//                    Log.d("B", child.className.toString())
+//
+//                    val list = child.findAccessibilityNodeInfosByText("START NOW")
+//                    for (node in list) {
+//                        node.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+//                    }
+//
+//                } else {
+//                    Log.d("A", "Child is Null")
+//                }
+//            }
+//        }
+
+
         event?.source?.apply {
 
-            val list: List<AccessibilityNodeInfo> = event.source.findAccessibilityNodeInfosByText("START NOW")
+            val list: List<AccessibilityNodeInfo> =
+                event.source.findAccessibilityNodeInfosByText("START NOW")
 
-            if(list.isNotEmpty()) {
+            if (list.isNotEmpty()) {
                 list.first().performAction(AccessibilityNodeInfo.ACTION_CLICK)
             }
 
@@ -27,10 +49,12 @@ class AutoStartAccessibilityService: AccessibilityService() {
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
 
         val accessibilityServiceInfo = AccessibilityServiceInfo()
-        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPE_VIEW_FOCUSED or AccessibilityEvent.TYPE_WINDOWS_CHANGED or AccessibilityEvent.TYPES_ALL_MASK
+        accessibilityServiceInfo.eventTypes =
+            AccessibilityEvent.TYPE_VIEW_FOCUSED or AccessibilityEvent.TYPE_WINDOWS_CHANGED or AccessibilityEvent.TYPES_ALL_MASK
 
         accessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN
-        accessibilityServiceInfo.notificationTimeout = 100
+        accessibilityServiceInfo.flags = AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS
+        accessibilityServiceInfo.notificationTimeout = 1000
 
         this.serviceInfo = accessibilityServiceInfo
 
